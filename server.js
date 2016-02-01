6d01a459fb7b5d5bd1f273af5ff7dcb86fcb4bd7
Tara Lerias <tlerias@gmail.com>
2016-01-31 22:41:39 -0500

    inital commit - add server and config files

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+var config = require('./server/config/config');
+
+var app = express();
+
+// Start server
+app.listen(config.port, config.ip, function () {
+  console.log('Express server listening on %s:%d, in %s mode', config.ip, config.port, app.get('env'));
+});
+
+// Expose app
+exports = module.exports = app;
